59cbb57dad2cf0184b2a628636dc7f5cb315c94c
ECOMT\npena <npena@ecomt.net>
2022-04-04 10:04:59 +0200

    creacion proyecto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; //service Worker en el tutorial
+import {createStore} from 'redux';
+
+
+//Store => estado globalizado (State)
+
+//Action => descripcion de lo que queremos que haga, no es lo mismo de lo que hará [contador, INCREMENTADOR]
+const increment = ()=>{
+  return{
+      type: 'INCREMENT'
+  }
+}
+
+const decrement=()=>{
+  return{
+    type: 'DECREMENT'
+  }
+}
+
+//Reducer => nos describe como la accion se tranaforma en un estado u otro
+const counter=(state=0, action )=>{
+    switch(action.type) { //cambiar la accion dependiendo del nombre
+      case "INCREMENT":
+        return state + 1; //aumenta el estado
+      case "DECREMENT":
+        return state - 1; //reduce el estado
+
+    }
+};
+
+let store= createStore(counter);
+
+//display in the console
+
+//Dispatch => manda la accion al reducer, el cual mira que hay que hacer
 
 ReactDOM.render(
   <React.StrictMode>
